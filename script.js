0f2aee127669954e97d03e46e2caf24d64b6854e
Arata <Josuancls21@gmail.com>
2023-12-27 21:39:38 +0700

    Final Commit
    Truncation
    Add + Remove Card
    Logo

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-// append card wanna be variable
-var cardDiv = document.getElementById("card-content")
+// Appending New Card
+function addChild() {
+  const container = document.getElementById("scroll_menu");
+  const template = document.getElementById("card_content");
+  const cloneDiv = template.cloneNode(true)
+  container.appendChild(cloneDiv);
+  const newDiv = document.createElement(template)
+  container.createEle(newDiv);
+}
+// Popping Card
+function popChild() {
+  const container = document.getElementById("scroll_menu");
+  if (container.lastElementChild) {
+      container.removeChild(container.lastElementChild);
+  }
+}
 
-const buttonRight = document.getElementById('slideRight');
-const buttonLeft = document.getElementById('slideLeft');
+// scroll button
+let buttonRight = document.getElementById('slideRight');
+let buttonLeft = document.getElementById('slideLeft');
+
+let scrollableDiv = document.getElementById("scroll_menu");
 
 buttonRight.onclick = function () {
   document.getElementById('scroll_menu').scrollLeft += 300;
@@ -11,6 +28,18 @@ buttonLeft.onclick = function () {
   document.getElementById('scroll_menu').scrollLeft -= 300;
 };
 
+// hiding button wannabe
+document.onscroll = function(){
+  if (scrollableDiv.scrollWidth <= scrollableDiv.clientWidth){
+    buttonLeft.style.display = 'none'
+    buttonRight.style.display = 'none'
+  }else{
+    buttonLeft.style.display = 'block'
+    buttonRight.style.display = 'block'
+  }
+}
+
+// to top button
 let mybutton = document.getElementById("toTopBtn");
 
 window.onscroll = function() {scrollFunction()};
@@ -28,6 +57,7 @@ function toTop() {
   document.documentElement.scrollTop = 0; 
 }
 
+// collapse button
 document.addEventListener("DOMContentLoaded", function() {
     const coll = document.querySelector(".collapsible");
     const content = document.querySelector(".footer-content");
